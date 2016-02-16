98909ed6ac778ab168828e0aa013da0e31a2fc12
ed <ucabeam@ucl.ac.uk>
2016-02-16 14:07:22 +0000

    Bids create change

--- api/bids/create/index.php
+++ api/bids/create/index.php
@@ -5,14 +5,16 @@
     include '../../sql_statements.php';
     include '../../helper.php';
 
-    //$id = intval($_GET['id']);
-    //$result = db_r_function(user($id));
-    
-    $user_id = $_POST['bidder_user_id'];
-    $auction_id = $_POST['bid_auction_id'];
+    $bidder_user_id = $_POST['bidder_user_id'];
+    $bid_auction_id = $_POST['bid_auction_id'];
     $bid_price = $_POST['bid_price'];
 
-    echo $user_id . ' ' . $auction_id . ' ' . $bid_price;
+//    echo $user_id . ' ' . $auction_id . ' ' . $bid_price;
+    
+    //$id = intval($_GET['id']);
+    $result = db_cud_function(bids_create($bidder_user_id, $bid_price, $bid_auction_id));
+    
+
     
     if ($result) {
         http_response_code(200);
