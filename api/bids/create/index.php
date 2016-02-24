8e406c23d2cd57c8f6a4c74738e77ec8e0eadb7a
ed <ucabeam@ucl.ac.uk>
2016-02-24 11:09:28 +0000

    Added helper function to validate get and post data.

--- api/bids/create/index.php
+++ api/bids/create/index.php
@@ -14,12 +14,17 @@
          $_SERVER['CONTENT_TYPE'] = $type;
 
     }
-    
-    $bidder_user_id = $_POST['bidder_user_id'];
-    $bid_auction_id = $_POST['bid_auction_id'];
-    $bid_price = $_POST['bid_price'];
 
-    $result = db_cud_function(bids_create($bidder_user_id, $bid_price, $bid_auction_id));
+    $post_data = validate_data("POST", array('bidder_user_id', 'bid_auction_id', 'bid_price'));
+    
+    $bidder_user_id = $post_data['bidder_user_id'];
+    $bid_auction_id = $post_data['bid_auction_id'];
+    $bid_price = $post_data['bid_price'];
+    
+    
+    var_dump($post_data);
+
+//    $result = db_cud_function(bids_create($bidder_user_id, $bid_price, $bid_auction_id));
 
     if ($result === TRUE) {
         http_response_code(200);
--- api/helper.php
+++ api/helper.php
@@ -66,3 +66,171 @@ function db_r_function($sql) {
     
     return $return;    
 }
+
+function validate_data($method, $names) {
+
+    $error = FALSE;
+    $returnData = array();
+    
+    
+    
+    for ($i=0; $i<count($names); $i++) {
+        
+        $returnData[$names[$i]] = array();
+        
+        $returnData[$names[$i]]["error"] = FALSE;
+        
+        $postValue = "";
+              
+        
+        if ($method($names[$i]) != NULL) {  
+            $postValue = mysql_escape_string($method($names[$i]));
+           
+        } else {
+            $returnData[$names[$i]]["error"] = $method . " variable '" . $names[$i] . "' is not set.";
+            $error = TRUE;
+            continue;
+        }
+
+        
+        if (preg_match('@user_id@', $names[$i]) || preg_match('@auction_id@', $names[$i])) {
+
+            if (!preg_match('@[0-9]+@', $method($names[$i]))) {
+                $returnData[$names[$i]]["error"] = "'" . $names[$i] . "' value is not an integer.";
+                $error = TRUE;
+                continue;                    
+            }
+
+        }
+            
+            
+        switch($names[$i]) {
+            
+            //Email case
+            case "email":
+                $email = $postValue;
+                
+                if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    $returnData[$names[$i]]["value"] = $email;
+                } else {
+                    $returnData[$names[$i]]["error"] = "Invalid email address.";
+                    $error = TRUE;
+                }
+                  
+                break;
+            
+            //Password case
+            case "password":
+               
+                $returnData["password"] = validate_password($postValue);
+                
+                break;
+                
+            default:
+                
+                $returnData[$names[$i]]["value"] = $postValue;
+                
+                break;
+                
+        }
+        
+    }
+    
+    if ($error) {
+        
+        $errorStr = "{error:[";
+        for ($i=0; $i<count($names); $i++) {
+
+            if ($returnData[$names[$i]]["error"]) {
+                $errorStr .= "\"" . $returnData[$names[$i]]["error"] . "\",";
+            }
+
+        }
+        
+        $errorStr[strlen($errorStr)-1] = "]";
+        $errorStr .= "}";
+        
+        die($errorStr);
+    } 
+    
+    return $returnData;
+    
+    
+}
+
+function validate_var($method, $var) {
+    
+    $returnData = array();
+    echo $method; 
+    if ($method($var) != NULL) {
+
+        $returnData = mysql_escape_string($method($var));           
+        $returnData["error"] = FALSE;
+    } else {
+        $returnData["error"] = $method . " variable '" . $var . "' is not set.";
+
+
+    }
+
+    return $returnData;
+        
+}
+
+function validate_id($id) {
+    
+    $returnData = array();
+    
+    if (preg_match('@user_id@', $id) || preg_match('@auction_id@', $id)) {
+
+        if (!preg_match('@[0-9]+@', $method($id))) {
+            $returnData["error"] = "'" . $id . "' value is not an integer.";
+            
+            return $returnData;
+        } 
+
+    }
+    
+    return mysql_escape_string($method($var));
+               
+}
+
+function validate_password($password) {
+    
+    $returnData = array();
+    
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $number    = preg_match('@[0-9]@', $password);
+
+    if(!$uppercase || !$lowercase || !$number || strlen($password) < 8) {
+        $returnData["error"] = ""; 
+    } else {
+        $returnData["value"] = $password;
+    }
+    
+    if(!$uppercase ) {
+        $returnData["error"] .= "Password must contain uppercase letter. ";
+    }
+
+    if (!$lowercase) {
+        $returnData["error"] .= "Password must contain lowercase letter. ";
+    }
+
+    if (!$number ) {
+        $returnData["error"] .= "Password must contain a number. ";
+    }
+
+    if (strlen($password) < 8) {
+        $returnData["error"] = "Password must have at least 8 characters. ";
+    }        
+    
+    return $returnData;
+}
+
+function POST($var) {
+    return $_POST[$var];
+}
+
+function GET($var) {
+    return $_GET[$var];
+}
