e329d618a3a94115e9af804fa21ab9021357136a
ed <ucabeam@ucl.ac.uk>
2016-02-26 16:32:08 +0000

    Auth edit

--- api/auth.php
+++ api/auth.php
@@ -4,7 +4,7 @@ $token = $_GET['access_token'];
 
 session_start();
 
-if ( $token != $_SESSION['access_token'] ) {
+if ( $token != $_SESSION['access_token'] || !isset($_SESSION['access_token'])) {
     http_response_code(401);
     die('Access token invalid ' + $token + " " + $_SESSION['access_token'] );
     
