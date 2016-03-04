5c87849a9d62772eb13f0cef2bf665f861b7b856
Jan Klug <jan.klug.15@ucl.ac.uk>
2016-03-04 17:07:46 +0000

    Update profile.js

--- app/js/controllers/profile.js
+++ app/js/controllers/profile.js
@@ -4,7 +4,7 @@ angular.module('controllers.profile', [])
 
 .controller('profileCtrl', ['$scope', '$http', function($scope, $http, $routeParams) {
 
-    var $scope.user_id = $routeParams.user_id
+    $scope.user_id = $routeParams.user_id
     
     function get_feedbacks(){
         $http.get(PATH_TO_API + 'feedback/for_user?user_id='+ $scope.user_id ).then(function(data){
