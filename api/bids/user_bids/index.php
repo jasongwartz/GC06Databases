50ad85a42577184a4f3aa2c5d119e7b7357fe7d8
ed <ucabeam@ucl.ac.uk>
2016-03-08 19:58:23 +0000

    Added bids page

--- api/bids/user_bids/index.php
+++ api/bids/user_bids/index.php
@@ -1,9 +1,15 @@
 <?php
-// bids/user_bids GET ?UNKNOWN UNUSED
+// bids/user_bids GET
 
-    include '../auth.php';
-    include '../sql_statements.php';
-    include '../helper.php';
+    //include '../auth.php';
+    include '../../sql_statements.php';
+    include '../../helper.php';
+
+    $get_data = validate_data("GET", array("bidder_user_id"));
+
+    $bidder_user_id = $get_data['bidder_user_id']['value'];
+//    echo bids_user_bids($bidder_user_id);
+    $result = db_r_function(bids_user_bids($bidder_user_id));
 
     if ($result) {
         http_response_code(200);
@@ -11,5 +17,5 @@
         
     } else {
         http_response_code(500);
-        echo '{error:"no data returned"}';
+        echo '{"error":"no data returned"}';
     }
